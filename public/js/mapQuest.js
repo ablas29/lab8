e3f7618a62fe6ee654b7ec3267e395ff2ed85044
Avery Blas <ablas@ucsd.edu>
2021-02-24 11:42:04 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'NPIDA3rVuy73GkuDDGYJdaXl1s2QMdVd';
+	var map = L.mapquest.map('map', {
+		center: [32.87860592839724, -117.23591455647711],
+		layers: L.mapquest.tileLayer('map'),
+		zoom:12,
+		zoomControl: false
+	});
+
+	L.marker([32.87860592839724, -117.23591455647711]).addTo(map);
 }
